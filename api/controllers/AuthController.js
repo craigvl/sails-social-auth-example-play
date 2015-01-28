ea9c9d46e96e127b09d2eb7e70acd0efe631588f
Craig <craig@192-168-1-103.tpgi.com.au>
2015-01-29 06:34:08 +1000

    Updates

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -10,84 +10,99 @@ var jwt = require('jwt-simple');
 
 module.exports = {
 
-  index: function(req, res) {
-    res.view();
-  },
+    index: function (req, res) {
+        res.view();
+    },
 
-  logout: function(req, res) {
-    req.logout();
-    res.redirect('/');
-  },
-
-  // http://developer.github.com/v3/
-  // http://developer.github.com/v3/oauth/#scopes
-  github: function(req, res) {
-    passport.authenticate('github', { failureRedirect: '/login' }, function(err, user) {
-      req.logIn(user, function(err) {
-        if (err) {
-          console.log(err);
-          res.view('500');
-          return;
-        }
-
+    logout: function (req, res) {
+        req.logout();
         res.redirect('/');
-        return;
-      });
-    })(req, res);
-  },
+    },
 
-  // https://developers.facebook.com/docs/
-  // https://developers.facebook.com/docs/reference/login/
-  facebook: function(req, res) {
-    passport.authenticate('facebook', { failureRedirect: '/login', scope: ['email'] }, function(err, user) {
-      req.logIn(user, function(err) {
-        if (err) {
-          console.log(err);
-          res.view('500');
-          return;
-        }
+    // http://developer.github.com/v3/
+    // http://developer.github.com/v3/oauth/#scopes
+    github: function (req, res) {
+        passport.authenticate('github', {
+            failureRedirect: '/login'
+        }, function (err, user) {
+            req.logIn(user, function (err) {
+                if (err) {
+                    console.log(err);
+                    res.view('500');
+                    return;
+                }
 
-        res.redirect('/');
-        return;
-      });
-    })(req, res);
-  },
+                res.redirect('/');
+                return;
+            });
+        })(req, res);
+    },
 
-  // https://developers.google.com/
-  // https://developers.google.com/accounts/docs/OAuth2Login#scope-param
-  google: function(req, res) {
-    passport.authenticate('google', { failureRedirect: '/login', scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/plus.profile.emails.read'] }, function(err, user) {
-      req.logIn(user, function(err) {
-        if (err) {
-          console.log(err);
-          res.view('500');
-          return;
-        }      
-        var token = jwt.encode({ id: user.id}, 'bunchy!Key!');  
-        res.status(200).send({
-        user: user.toJSON(),
-        token:token
-    });
-       // res.redirect('/');
-    //    return;
-      });
-    })(req, res);
-  },
+    // https://developers.facebook.com/docs/
+    // https://developers.facebook.com/docs/reference/login/
+    facebook: function (req, res) {
+        passport.authenticate('facebook', {
+            failureRedirect: '/login',
+            scope: ['email']
+        }, function (err, user) {
+            req.logIn(user, function (err) {
+                if (err) {
+                    console.log(err);
+                    res.view('500');
+                    return;
+                }
 
-  // https://apps.twitter.com/
-  // https://apps.twitter.com/app/new
-  twitter: function(req, res) {
-    passport.authenticate('twitter', { failureRedirect: '/login' }, function(err, user) {
-      req.logIn(user, function(err) {
-        if (err) {
-          console.log(err);
-          res.view('500');
-          return;
-        }
+                var token = jwt.encode({
+                    id: user.id
+                }, 'bunchy!Key!');
+                res.status(200).send({
+                    user: user.toJSON(),
+                    token: token
+                });
+            });
+        })(req, res);
+    },
 
-        res.redirect('/');
-        return;
-      });
-    })(req, res);
-  }
+    // https://developers.google.com/
+    // https://developers.google.com/accounts/docs/OAuth2Login#scope-param
+    google: function (req, res) {
+        passport.authenticate('google', {
+            failureRedirect: '/login',
+            scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/plus.profile.emails.read']
+        }, function (err, user) {
+            req.logIn(user, function (err) {
+                if (err) {
+                    console.log(err);
+                    res.view('500');
+                    return;
+                }
+                var token = jwt.encode({
+                    id: user.id
+                }, 'bunchy!Key!');
+                res.status(200).send({
+                    user: user.toJSON(),
+                    token: token
+                });
+            });
+        })(req, res);
+    },
+
+    // https://apps.twitter.com/
+    // https://apps.twitter.com/app/new
+    twitter: function (req, res) {
+        passport.authenticate('twitter', {
+            failureRedirect: '/login'
+        }, function (err, user) {
+            req.logIn(user, function (err) {
+                if (err) {
+                    console.log(err);
+                    res.view('500');
+                    return;
+                }
+
+                res.redirect('/');
+                return;
+            });
+        })(req, res);
+    }
 };
--- config/express.js
+++ config/express.js
@@ -1,50 +1,54 @@
-var passport = require('passport')
-    , GitHubStrategy = require('passport-github').Strategy
-    , FacebookStrategy = require('passport-facebook').Strategy
-    , GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
-    , TwitterStrategy = require('passport-twitter').Strategy;
+var passport = require('passport'),
+    GitHubStrategy = require('passport-github').Strategy,
+    FacebookStrategy = require('passport-facebook').Strategy,
+    GoogleStrategy = require('passport-google-oauth').OAuth2Strategy,
+    TwitterStrategy = require('passport-twitter').Strategy;
 
 
-var verifyHandler = function(token, tokenSecret, profile, done) {
-  process.nextTick(function() {
+var verifyHandler = function (token, tokenSecret, profile, done) {
+    process.nextTick(function () {
 
-    User.findOne({uid: profile.id}, function(err, user) {
-      if (user) {
-        return done(null, user);
-      } else {
+        User.findOne({
+            uid: profile.id
+        }, function (err, user) {
+            if (user) {
+                return done(null, user);
+            } else {
 
-        var data = {
-          provider: profile.provider,
-          uid: profile.id,
-          name: profile.displayName
-        };
+                var data = {
+                    provider: profile.provider,
+                    uid: profile.id,
+                    name: profile.displayName
+                };
 
-        if (profile.emails && profile.emails[0] && profile.emails[0].value) {
-          data.email = profile.emails[0].value;
-        }
-        if (profile.name && profile.name.givenName) {
-          data.firstname = profile.name.givenName;
-        }
-        if (profile.name && profile.name.familyName) {
-          data.lastname = profile.name.familyName;
-        }
+                if (profile.emails && profile.emails[0] && profile.emails[0].value) {
+                    data.email = profile.emails[0].value;
+                }
+                if (profile.name && profile.name.givenName) {
+                    data.firstname = profile.name.givenName;
+                }
+                if (profile.name && profile.name.familyName) {
+                    data.lastname = profile.name.familyName;
+                }
 
-        User.create(data, function(err, user) {
-          return done(err, user);
+                User.create(data, function (err, user) {
+                    return done(err, user);
+                });
+            }
         });
-      }
     });
-  });
 };
 
-passport.serializeUser(function(user, done) {
-  done(null, user.uid);
+passport.serializeUser(function (user, done) {
+    done(null, user.uid);
 });
 
-passport.deserializeUser(function(uid, done) {
-  User.findOne({uid: uid}, function(err, user) {
-    done(err, user);
-  });
+passport.deserializeUser(function (uid, done) {
+    User.findOne({
+        uid: uid
+    }, function (err, user) {
+        done(err, user);
+    });
 });
 
 /**
@@ -55,113 +59,113 @@ passport.deserializeUser(function(uid, done) {
  */
 module.exports.http = {
 
-  customMiddleware: function(app) {
+    customMiddleware: function (app) {
 
-    passport.use(new GitHubStrategy({
-      clientID: "YOUR_CLIENT_ID",
-      clientSecret: "YOUR_CLIENT_SECRET",
-      callbackURL: "http://localhost:1337/auth/github/callback"
-    }, verifyHandler));
+        passport.use(new GitHubStrategy({
+            clientID: "YOUR_CLIENT_ID",
+            clientSecret: "YOUR_CLIENT_SECRET",
+            callbackURL: "http://localhost:1337/auth/github/callback"
+        }, verifyHandler));
 
-    passport.use(new FacebookStrategy({
-      clientID: "YOUR_CLIENT_ID",
-      clientSecret: "YOUR_CLIENT_SECRET",
-      callbackURL: "http://localhost:1337/auth/facebook/callback"
-    }, verifyHandler));
+        passport.use(new FacebookStrategy({
+            clientID: "1403149849923216 ",
+            clientSecret: "304db854c24fa4c573eaa15dbb65c26e",
+            callbackURL: "http://localhost:1337/auth/facebook/callback"
+        }, verifyHandler));
 
-    passport.use(new GoogleStrategy({
-      clientID: '549020993769-tn974vfkrovsr1k4g65135k6m02vec6j.apps.googleusercontent.com',
-      clientSecret: 'q_9Ll4BLQY6xmuqHKSI932JR',
-      callbackURL: 'http://localhost:1337/auth/google/callback'
-    }, verifyHandler));
+        passport.use(new GoogleStrategy({
+            clientID: '549020993769-tn974vfkrovsr1k4g65135k6m02vec6j.apps.googleusercontent.com',
+            clientSecret: 'q_9Ll4BLQY6xmuqHKSI932JR',
+            callbackURL: 'http://localhost:1337/auth/google/callback'
+        }, verifyHandler));
 
-    passport.use(new TwitterStrategy({
-      consumerKey: 'YOUR_CLIENT_ID',
-      consumerSecret: 'YOUR_CLIENT_SECRET',
-      callbackURL: 'http://localhost:1337/auth/twitter/callback'
-    }, verifyHandler));
+        passport.use(new TwitterStrategy({
+            consumerKey: 'YOUR_CLIENT_ID',
+            consumerSecret: 'YOUR_CLIENT_SECRET',
+            callbackURL: 'http://localhost:1337/auth/twitter/callback'
+        }, verifyHandler));
 
-    app.use(passport.initialize());
-    app.use(passport.session());
-  }
+        app.use(passport.initialize());
+        app.use(passport.session());
+    }
 
-  // Completely override Express middleware loading.
-  // If you only want to override the bodyParser, cookieParser
-  // or methodOverride middleware, see the appropriate keys below.
-  // If you only want to override one or more of the default middleware,
-  // but keep the order the same, use the `middleware` key.
-  // See the `http` hook in the Sails core for the default loading order.
-  //
-  // loadMiddleware: function( app, defaultMiddleware, sails ) { ... }
-
-
-  // Override one or more of the default middleware (besides bodyParser, cookieParser)
-  //
-  // middleware: {
-  //    session: false, // turn off session completely for HTTP requests
-  //    404: function ( req, res, next ) { ... your custom 404 middleware ... }
-  // }
+    // Completely override Express middleware loading.
+    // If you only want to override the bodyParser, cookieParser
+    // or methodOverride middleware, see the appropriate keys below.
+    // If you only want to override one or more of the default middleware,
+    // but keep the order the same, use the `middleware` key.
+    // See the `http` hook in the Sails core for the default loading order.
+    //
+    // loadMiddleware: function( app, defaultMiddleware, sails ) { ... }
 
 
-  // The middleware function used for parsing the HTTP request body.
-  // (this most commonly comes up in the context of file uploads)
-  //
-  // Defaults to a slightly modified version of `express.bodyParser`, i.e.:
-  // If the Connect `bodyParser` doesn't understand the HTTP body request
-  // data, Sails runs it again with an artificial header, forcing it to try
-  // and parse the request body as JSON.  (this allows JSON to be used as your
-  // request data without the need to specify a 'Content-type: application/json'
-  // header)
-  //
-  // If you want to change any of that, you can override the bodyParser with
-  // your own custom middleware:
-  // bodyParser: function customBodyParser (options) { ... return function(req, res, next) {...}; }
-  //
-  // Or you can always revert back to the vanilla parser built-in to Connect/Express:
-  // bodyParser: require('express').bodyParser,
-  //
-  // Or to disable the body parser completely:
-  // bodyParser: false,
-  // (useful for streaming file uploads-- to disk or S3 or wherever you like)
-  //
-  // WARNING
-  // ======================================================================
-  // Multipart bodyParser (i.e. express.multipart() ) will be removed
-  // in Connect 3 / Express 4.
-  // [Why?](https://github.com/senchalabs/connect/wiki/Connect-3.0)
-  //
-  // The multipart component of this parser will be replaced
-  // in a subsequent version of Sails (after v0.10, probably v0.11) with:
-  // [file-parser](https://github.com/balderdashy/file-parser)
-  // (or something comparable)
-  //
-  // If you understand the risks of using the multipart bodyParser,
-  // and would like to disable the warning log messages, uncomment:
-  // silenceMultipartWarning: true,
-  // ======================================================================
+    // Override one or more of the default middleware (besides bodyParser, cookieParser)
+    //
+    // middleware: {
+    //    session: false, // turn off session completely for HTTP requests
+    //    404: function ( req, res, next ) { ... your custom 404 middleware ... }
+    // }
 
 
-  // Cookie parser middleware to use
-  //			(or false to disable)
-  //
-  // Defaults to `express.cookieParser`
-  //
-  // Example override:
-  // cookieParser: (function customMethodOverride (req, res, next) {})(),
+    // The middleware function used for parsing the HTTP request body.
+    // (this most commonly comes up in the context of file uploads)
+    //
+    // Defaults to a slightly modified version of `express.bodyParser`, i.e.:
+    // If the Connect `bodyParser` doesn't understand the HTTP body request
+    // data, Sails runs it again with an artificial header, forcing it to try
+    // and parse the request body as JSON.  (this allows JSON to be used as your
+    // request data without the need to specify a 'Content-type: application/json'
+    // header)
+    //
+    // If you want to change any of that, you can override the bodyParser with
+    // your own custom middleware:
+    // bodyParser: function customBodyParser (options) { ... return function(req, res, next) {...}; }
+    //
+    // Or you can always revert back to the vanilla parser built-in to Connect/Express:
+    // bodyParser: require('express').bodyParser,
+    //
+    // Or to disable the body parser completely:
+    // bodyParser: false,
+    // (useful for streaming file uploads-- to disk or S3 or wherever you like)
+    //
+    // WARNING
+    // ======================================================================
+    // Multipart bodyParser (i.e. express.multipart() ) will be removed
+    // in Connect 3 / Express 4.
+    // [Why?](https://github.com/senchalabs/connect/wiki/Connect-3.0)
+    //
+    // The multipart component of this parser will be replaced
+    // in a subsequent version of Sails (after v0.10, probably v0.11) with:
+    // [file-parser](https://github.com/balderdashy/file-parser)
+    // (or something comparable)
+    //
+    // If you understand the risks of using the multipart bodyParser,
+    // and would like to disable the warning log messages, uncomment:
+    // silenceMultipartWarning: true,
+    // ======================================================================
 
 
-  // HTTP method override middleware
-  //			(or false to disable)
-  //
-  // This option allows artificial query params to be passed to trick
-  // Sails into thinking a different HTTP verb was used.
-  // Useful when supporting an API for user-agents which don't allow
-  // PUT or DELETE requests
-  //
-  // Defaults to `express.methodOverride`
-  //
-  // Example override:
-  // methodOverride: (function customMethodOverride (req, res, next) {})()
+    // Cookie parser middleware to use
+    //			(or false to disable)
+    //
+    // Defaults to `express.cookieParser`
+    //
+    // Example override:
+    // cookieParser: (function customMethodOverride (req, res, next) {})(),
+
+
+    // HTTP method override middleware
+    //			(or false to disable)
+    //
+    // This option allows artificial query params to be passed to trick
+    // Sails into thinking a different HTTP verb was used.
+    // Useful when supporting an API for user-agents which don't allow
+    // PUT or DELETE requests
+    //
+    // Defaults to `express.methodOverride`
+    //
+    // Example override:
+    // methodOverride: (function customMethodOverride (req, res, next) {})()
 };
 
 
@@ -183,7 +187,7 @@ module.exports.http = {
  */
 module.exports.cache = {
 
-  // The number of seconds to cache files being served from disk
-  // (only works in production mode)
-  maxAge: 31557600000
+    // The number of seconds to cache files being served from disk
+    // (only works in production mode)
+    maxAge: 31557600000
 };
